e6a562b4975e379332bb4576af8dec665e5d4634
AlexDuOl <contactogatovagabundo@gmail.com>
2018-05-14 10:53:31 -0500

    Ajuste de archivo JS

--- js/app.js
+++
@@ -1,8 +0,0 @@
-/*var primeraseccion = document.getElementById('primeraseccion');
-var addlist = document.getElementById('addlist');*/
-document.getElementById("#addlist").addEventListener("clik", primero);
-
-function primero(evento) {
-  alert("hola")
-  document.getElementById("#addlist").removeEventListener("clik", primero);
-}
