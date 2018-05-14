81fd44cb449750e44cae88b15413b3eeec6f6937
AlexDuOl <contactogatovagabundo@gmail.com>
2018-05-13 21:19:20 -0500

    Agregando js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-{
+/*var primeraseccion = document.getElementById('primeraseccion');
+var addlist = document.getElementById('addlist');*/
+document.getElementById("#addlist").addEventListener("clik", primero);
 
+function primero(evento) {
+  alert("hola")
+  document.getElementById("#addlist").removeEventListener("clik", primero);
 }
