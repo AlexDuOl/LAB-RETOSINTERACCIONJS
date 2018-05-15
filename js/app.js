dcd6617291b7c6a39a12c8205bcb5abe538fa166
AlexDuOl <contactogatovagabundo@gmail.com>
2018-05-15 15:22:28 -0500

    Subiendo archivos modificados, ordenando la identacion

--- js/app.js
+++ js/app.js
@@ -1,43 +1,73 @@
-var container = document.getElementsByClassName("container")[0];
 var addlist = document.getElementById("addlist");
-var list = document.getElementById("list");
 var boton = document.createElement('button');
 var botonX = document.createElement('button');
+var container = document.getElementsByClassName("container")[0];
+var list = document.getElementById("list");
+
 /*PRIMERA VERSION*/
 addlist.addEventListener("click", function(){
+  boton.className = "guardar";
+  botonX.className = "botonCancel";
   container.className = "square";
   list.className = "newInput";
 
   boton.type = "button";
-  boton.className = "guardar";
   boton.appendChild(document.createTextNode("Guardar"));
   container.appendChild(boton);
 
   botonX.type = "button";
-  botonX.className = "botonCancel";
   botonX.innerHTML = "&times;";
   container.appendChild(botonX);
+});
 
 /*SEGUNDA VERSION*/
 boton.addEventListener("click", second);
-});
 
 function second(){
-  var newName = document.getElementById("list").value;
-  newName.value = " ";
   var newText = document.createTextNode(newName);
   var newElement = document.createElement("h2");
+  var newList = document.createTextNode('Añadir una tarea');
+  var newLink = document.createElement("a");
+  var newName = document.getElementById("list").value;
+
   newElement.className = 'newElement';
+
+  newName.value = " ";
+
   newElement.appendChild(newText);
   addlist.appendChild(newElement);
   container.appendChild(addlist);
-  boton.style.display = 'none';
-  botonX.style.display = 'none';
-  list.style.display = 'none';
-  var newList = document.createTextNode('Añadir una tarea');
-  var newLink = document.createElement("a");
+
   newLink.className = 'link';
   newLink.appendChild(newList);
   container.appendChild(newLink);
+
+  boton.style.display = 'none';
+  botonX.style.display = 'none';
+  list.style.display = 'none';
+
   boton.removeEventListener('click', second);
  }
+
+ /*TERCERA VERSION*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
